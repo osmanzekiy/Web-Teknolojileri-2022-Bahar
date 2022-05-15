dbf73eb69a6cb31483db7ee4870cc1f6f4f8069e
osmanzekiy <104172490+osmanzekiy@users.noreply.github.com>
2022-05-15 23:37:03 +0300

    Add files via upload

--- web-proje/control.php
+++ web-proje/control.php
@@ -3,12 +3,12 @@ $eposta=$_POST["mail"];
 $sifre=$_POST["pass"];
 
 if($sifre=="g211210024" && $eposta=="g211210024@sakarya.edu.tr"){
-    header('Location: http://localhost/web-proje/hosgeldiniz.html');
+    header('Location: http://qweasd.byethost9.com/web-proje/hosgeldiniz.html');
 }
 else{
     
     
-    header('Location: http://localhost/web-proje/login.html');
+    header('Location: http://qweasd.byethost9.com/web-proje/login.html');
    
 }
 
